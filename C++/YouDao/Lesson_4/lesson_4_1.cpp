4127b27d8ffb5f9c1fd17a7f18d02ff5bb343636
yuzheyaun <13261317705@sina.cn>
2023-06-02 20:38:40 +0800

    add youdao lesson_4_1(finish)

--- C++/YouDao/Lesson_4/lesson_4_1.cpp
+++ C++/YouDao/Lesson_4/lesson_4_1.cpp
@@ -1,37 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-int mapf[11][11], tmp[11][11], energy;
+long long a[15][15], f[15][15], n, en;
+
 int main(){
-    int a,b;
-    // cin >> a >> b;
-    a = 25;
-    b = 6;
-    energy = b;
-    int x, y, z;
-    // while(true){
-    //     cin >> x >> y >> z;
-    //     if(!x && !y && !z) break;
-    //     mapf[x][y] = z;
-    // }
-    mapf[1][1] = 12;
-    mapf[2][3] = 6;
-    mapf[3][5] = 7;
-    mapf[4][2] = 11;
-    mapf[5][5] = 19;
-    mapf[3][6] = 4;
-    mapf[6][3] = 15;
-    mapf[4][4] = 7;
-    x = 1;
-    y = 1;
-    for(int i = 1; i <= a; i ++){
-        for(int j = a; j >= 1; j --){
-            tmp[i][j] = max(mapf[i - 1][j], mapf[i][j + 1]);
-            cout << tmp[i][j] << endl;
+    cin >> en >> n;
+    while(true){
+        int x, y, z;
+        cin >> x >> y >> z;
+        if(!x && !y && !z) break;
+        a[x][y] = z;
+    }
+    for(int i = 1; i <= n; i ++){
+        for(int j = n; j >= 1; j --){
+            f[i][j] = max(f[i - 1][j],f[i][j + 1]) + a[i][j];
         }
     }
-    if(tmp[a][1] >= energy)
-        cout << "no";
-    else
-        cout << tmp[a][1];
+    if(en >= f[n][1]) cout << f[n][1];
+    else cout << "no";
     return 0;
 }
