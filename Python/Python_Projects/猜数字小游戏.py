b9eea2c2a290d9a8fb707c75a8528994fb8a55fb
yuzheyaun <13261317705@sina.cn>
2023-06-04 09:43:56 +0800

    add python

--- Python/Python_Projects/猜数字小游戏.py
+++ Python/Python_Projects/猜数字小游戏.py
@@ -1,8 +1,18 @@
-temp = input("猜一下小甲鱼想的是什么数字\n")
-guess = int(temp)
-if guess == 8:
-    print("你是小甲鱼心里的蛔虫吗？！")
-    print("哼，猜中了也被有奖励！")
-else:
-    print("猜错啦，小甲鱼想的是8！")
+
+import random
+counts = 3
+answer = random.randint(1, 10)
+while counts > 0 :
+    temp = input("猜一下小甲鱼想的是什么数字\n")
+    guess = int(temp)
+    if guess == answer:
+        print("你是小甲鱼心里的蛔虫吗？！")
+        print("哼，猜中了也被有奖励！")
+        break
+    else:
+        if guess < answer:
+            print("小啦~")
+        else:
+            print("大啦~")
+        counts = counts - 1
 print("游戏结束，不玩啦^_^")
