d0fd7db0ff78a06e6b3a361eac861b714aa02dab
yuzheyaun <13261317705@sina.cn>
2023-06-07 18:27:28 +0800

    change rust files

---
+++ Rust/Projext1/target/debug/Projext1.d
@@ -0,0 +1,1 @@
+C:\Users\13261\OneDrive\Document\Document\Programe\Rust\Projext1\target\debug\Projext1.exe: C:\Users\13261\OneDrive\Document\Document\Programe\Rust\Projext1\src\main.rs
---
+++ Rust/Projext1/target/debug/deps/Projext1.d
@@ -0,0 +1,5 @@
+C:\Users\13261\OneDrive\Document\Document\Programe\Rust\Projext1\target\debug\deps\Projext1.exe: src\main.rs
+
+C:\Users\13261\OneDrive\Document\Document\Programe\Rust\Projext1\target\debug\deps\Projext1.d: src\main.rs
+
+src\main.rs:
