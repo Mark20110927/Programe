21bf88a58dfc02977ac5f423d294907cf1eadacb
yuzheyaun <13261317705@sina.cn>
2023-06-10 07:51:53 +0800

    add youdao lesson_5_home_2(1)

--- C++/YouDao/Lesson_5/lesson_5_homework_2.cpp
+++ C++/YouDao/Lesson_5/lesson_5_homework_2.cpp
@@ -1,6 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, x1, x2;
+struct srore{
+    int sum;
+    int chinese;
+    int maths;
+    int english;
+}a[105];
 int main(){
-    
+    cin >> n;
+    for(int i = 1; i <= n; i ++)
+        cin >> a[i].sum >> a[i].chinese >> a[i].maths >> a[i].english;
+    cin >> x1 >> x2;
+    int l = 1, mid, r = n, ans1, ans2;
+    while(l <= r){
+        mid = (l + r) / 2;
+        if(a[mid].sum == x1) ans1 = mid;
+        else if(a[mid].sum > x1) r = mid - 1;
+        else l = mid + 1;
+    }
+    l = 1, r = n;
+    while(l <= r){
+        mid = (l + r) / 2;
+        if(a[mid].sum == x2) ans2 = mid;
+        else if(a[mid].sum > x2) r = mid - 1;
+        else l = mid + 1;
+    }
+    int res1 = a[ans2].maths - a[ans1].maths;
+    int res2 = a[ans2].english - a[ans1].english;
+    printf("%d %d", res1, res2);
     return 0;
 }
