91483aaf28cf73149ca3202c7a8950852fcd6cd3
yuzheyaun <13261317705@sina.cn>
2023-06-09 21:06:22 +0800

    add youdao lesson_5_5(finish)

--- C++/YouDao/Lesson_5/lesson_5_5.cpp
+++ C++/YouDao/Lesson_5/lesson_5_5.cpp
@@ -1,6 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, m, a[100005], x;
 int main(){
-    
+    cin >> n;
+    for(int i = 0; i < n; i ++) cin >> a[i];
+    cin >> m;
+    while(m --){
+        cin >> x;
+        if(x < a[0]){
+            cout << a[0] << endl;
+            continue;
+        }
+        if(x > a[n - 1]){
+            cout << a[n - 1] << endl;
+            continue;
+        }
+        int l = 0, mid, r = n - 1, ans = 0;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(a[mid] == x){
+                cout << x << endl;
+                ans = 1;
+                break;
+            }
+            else if(a[mid] > x) r = mid - 1;
+            else l = mid + 1;
+        }
+        if(!ans){
+            if(x - a[r] <= a[l] - x) cout << a[r] << endl;
+            else cout << a[l] << endl;
+        }
+    }
     return 0;
 }
