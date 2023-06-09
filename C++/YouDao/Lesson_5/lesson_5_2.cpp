17086f1b4096d1312e26912572e391ab02a988d4
yuzheyaun <13261317705@sina.cn>
2023-06-09 19:13:26 +0800

    add youdao lesson_5_2(1)

--- C++/YouDao/Lesson_5/lesson_5_2.cpp
+++ C++/YouDao/Lesson_5/lesson_5_2.cpp
@@ -1,6 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, a[100005], m;
 int main(){
-
+    cin >> n;
+    for(int i = 1; i <= n; i ++)
+        cin >> a[i];
+    cin >> m;
+    sort(a + 1, a + n);
+    for(int i = 1; i < n; i ++){
+        int l = i + 1, mid, r = n, x = m - a[i];
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(mid == x){
+                cout << a[i] << ' ' << mid;
+                return 0;
+            }
+            else if(mid > x){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+    }
+    cout << "No";
     return 0;
 }
