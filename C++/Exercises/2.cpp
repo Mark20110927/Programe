b9758dfda3400aed1c9bdbbd12a27f8bf5787df1
yuzheyaun <13261317705@sina.cn>
2023-06-10 16:17:15 +0800

    add c++ exercise_2(1)

--- C++/Exercises/2.cpp
+++ C++/Exercises/2.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 //顺序查找-直方图
+int n, a[10005], b[10005], maxf = 0;
 int main(){
-    
+    cin >> n;
+    for(int i = 1; i <= n; i ++) cin >> a[i];
+    for(int i = 1; i <= n; i ++){
+        if(a[i] > maxf){
+            b[a[i]] ++;
+            maxf = a[i];
+        }
+        else break;
+    }
+    for(int i = 1; i <= n; i ++) cout << b[i] << endl;
     return 0;
 }
