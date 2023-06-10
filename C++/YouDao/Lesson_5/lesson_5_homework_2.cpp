0a8370c632c501af0cda48fa951e74b7bce67bd9
yuzheyaun <13261317705@sina.cn>
2023-06-10 11:47:42 +0800

    add youdao lesson_5_2(3)

--- C++/YouDao/Lesson_5/lesson_5_homework_2.cpp
+++ C++/YouDao/Lesson_5/lesson_5_homework_2.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n, x1, x2;
+bool flag1, flag2;
 struct srore{
     int sum;
     int chinese;
@@ -16,6 +17,7 @@ int main(){
     while(l <= r){
         mid = (l + r) / 2;
         if(a[mid].sum == x1){
+            flag1 = true;
             ans1 = mid;
             break;
         }
@@ -26,14 +28,26 @@ int main(){
     while(l <= r){
         mid = (l + r) / 2;
         if(a[mid].sum == x2){
+            flag2 = true;
             ans2 = mid;
             break;
         }
         else if(a[mid].sum > x2) r = mid - 1;
         else l = mid + 1;
     }
-    int res1 = a[ans2].maths - a[ans1].maths;
-    int res2 = a[ans2].english - a[ans1].english;
-    cout << res1 << ' ' << res2;
+    if(flag1 && flag2){
+        int res1, res2;
+        if(a[ans1].maths > a[ans2].maths)
+            res1 = a[ans1].maths - a[ans2].maths;
+        else
+            res1 = a[ans2].maths - a[ans1].maths;
+        if(a[ans1].english > a[ans2].english)
+            res2 = a[ans1].english - a[ans2].english;
+        else
+            res2 = a[ans2].english - a[ans1].english;
+        cout << res1 << ' ' << res2;
+        return 0;
+    }
+    cout << -1;
     return 0;
 }
