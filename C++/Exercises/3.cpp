df5328143461e2f1b2155ba9bbff12f7c6d416fc
yuzheyaun <13261317705@sina.cn>
2023-06-10 16:42:31 +0800

    add c++ exercise_3(finish)

--- C++/Exercises/3.cpp
+++ C++/Exercises/3.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 int k;
 string s;
-int a[1000];
+int a[256];
 int main(){
     cin >> k;
     cin >> s;
     int len = s.length();
-    a[s[0]] ++;
-    for(int i = 1; i < len; i ++){
-        if(a[i - 1] == a[i])
-            a[s[i]] ++;
-    }
-    for(int i = 1; i <= 1000; i ++){
-        if(a[i] >= k){
-            cout << char(i);
+    for(int i = 0; i < len; i ++){
+        if(i == 0 || s[i] != s[i - 1])
+            a[s[i]] = 0;
+        a[s[i]] ++;
+        if(a[s[i]] >= k){
+            cout << s[i];
             return 0;
         }
     }
+    cout << "No";
     return 0;
 }
