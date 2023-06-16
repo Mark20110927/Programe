6054441dfb57913592973b96ae0f13c55bb14ef2
yuzheyaun <13261317705@sina.cn>
2023-06-16 18:47:21 +0800

    add youdao lesson_6_1(1)

--- C++/YouDao/Lesson_6/lesson_6_1.cpp
+++ C++/YouDao/Lesson_6/lesson_6_1.cpp
@@ -1,6 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[1000005], n, c, sum;
 int main(){
-
+    cin >> n >> c;
+    for(int i = 1; i <= n; i ++) cin >> a[i];
+    sort(a, a + n);
+    for(int i = 1; i <= n; i ++){
+        int x = a[i] + c;
+        int l = i + 1, mid, r = n, ans1 = -1, ans2 = 0;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(a[mid] == x){
+                ans1 = mid;
+                break;
+            }
+            else if(a[mid] < x) l = mid + 1;
+            else r = mid - 1;
+        }
+        l = i - 1, r = n;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(a[mid] == x){
+                ans2 = mid;
+                break;
+            }
+            else if(a[mid] < x) l = mid + 1;
+            else r = mid - 1;
+        }
+        if(ans1 != -1 && ans2 != -1) sum += ans2 - ans1 + 1;
+        else if(ans1 != -1) sum ++;
+    }
+    cout << sum;
     return 0;
 }
