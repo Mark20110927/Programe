ecb2b4f7d06a30c73ec672fed5a139ec734cb618
yuzheyaun <13261317705@sina.cn>
2023-06-17 07:10:28 +0800

    add youdao lesson_6_home_1(1)

--- C++/YouDao/Lesson_6/lesson_6_homework_1.cpp
+++ C++/YouDao/Lesson_6/lesson_6_homework_1.cpp
@@ -1,6 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[100005], b[100005], m, n, maxf, sum;
 int main(){
-    
+    cin >> m >> n;
+    for(int i = 1; i <= m; i ++){
+        cin >> a[i];
+        if(a[i] > maxf) maxf = a[i];
+    }
+    for(int i = 1; i <= n; i ++) cin >> b[i];
+    sort(a + 1, a + 1 + m);
+    sort(b + 1, b + 1 + n);
+    int l = 0, r = maxf, mid, res;
+    for(int i = 1; i <= n; i ++){
+        res = 1e6;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(a[i] == mid){
+                res = 0;
+                break;
+            }
+            else if(a[i] > mid){
+                l = mid + 1;
+                if(abs(a[i] - mid) < res) res = abs(a[i] - mid);
+            }
+        }
+        sum += res;
+    }
+    cout << sum;
     return 0;
 }
