20a825ea935e66a6ac900fcadfc8fdc859f2ef45
yuzheyaun <13261317705@sina.cn>
2023-06-17 06:50:59 +0800

    add youdao lesson_6_4(finish)

--- C++/YouDao/Lesson_6/lesson_6_4.cpp
+++ C++/YouDao/Lesson_6/lesson_6_4.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+double n, e;
+int m;
 int main(){
-    
+    cin >> n >> e >> m;
+    double l = 0, r = 5, mid;
+    while((r - l) > 0.00001){
+        mid = (l + r) / 2;
+        double a = n;
+        for(int i = 1; i <= m; i ++)
+            a = a * (1 + mid) - e;
+        if(a == 0) break;
+        else if(a > 0) r = mid;
+        else l = mid;
+    }
+    cout << floor(mid * 1000) / 1000 * 100;
     return 0;
 }
