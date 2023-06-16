0faa76ffc9c238faa95cb57c4f2aac3ef0eb740b
yuzheyaun <13261317705@sina.cn>
2023-06-17 07:13:35 +0800

    add youdao lesson_6_home_1(2)

--- C++/YouDao/Lesson_6/lesson_6_homework_1.cpp
+++ C++/YouDao/Lesson_6/lesson_6_homework_1.cpp
@@ -11,8 +11,10 @@ int main(){
     sort(a + 1, a + 1 + m);
     sort(b + 1, b + 1 + n);
     int l = 0, r = maxf, mid, res;
+    bool flag = 0;
     for(int i = 1; i <= n; i ++){
         res = 1e6;
+        flag = 0;
         while(l <= r){
             mid = (l + r) / 2;
             if(a[i] == mid){
@@ -22,9 +24,17 @@ int main(){
             else if(a[i] > mid){
                 l = mid + 1;
                 if(abs(a[i] - mid) < res) res = abs(a[i] - mid);
+                flag = true;
+                break;
+            }
+            else{
+                r = mid - 1;
+                if(abs(a[i] - mid) < res) res = abs(a[i] - mid);
+                flag = true;
+                break;
             }
         }
-        sum += res;
+        if(!flag) sum += res;
     }
     cout << sum;
     return 0;
