d73a23bab878011b3aa4a9a7ad114047c922aaad
yuzheyaun <13261317705@sina.cn>
2023-06-17 07:46:38 +0800

    add youdao lesson_6_home_1(finish)

--- C++/YouDao/Lesson_6/lesson_6_homework_1.cpp
+++ C++/YouDao/Lesson_6/lesson_6_homework_1.cpp
@@ -1,39 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int a[100005], b[100005], m, n, maxf, sum;
+const int N = 100010;
+int a[N], b[N];
+int n, m;
 int main(){
-    cin >> m >> n;
-    for(int i = 1; i <= m; i ++){
-        cin >> a[i];
-        if(a[i] > maxf) maxf = a[i];
-    }
-    for(int i = 1; i <= n; i ++) cin >> b[i];
-    sort(a + 1, a + 1 + m);
-    sort(b + 1, b + 1 + n);
-    int l = 0, r = maxf, mid, res;
-    for(int i = 1; i <= n; i ++){
-        res = 1e6;
-        l = 0, r = maxf;
-        while(l <= r){
-            mid = (l + r) / 2;
-            if(a[i] == mid){
-                res = 0;
-                break;
-            }
-            else if(a[i] > mid){
-                l = mid + 1;
-            }
-            else{
-                r = mid - 1;
-            }
+    cin >> n >> m;
+    for (int i = 0; i < n; i ++ ) cin >> a[i];
+    for (int i = 0; i < m; i ++ ) cin >> b[i];
+    sort(a, a + n);
+    sort(b, b + m);
+    int res = 0;
+    for (int i = 0; i < m; i ++ ){
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            int mid = l + r >> 1;
+            if (a[mid] < b[i]) l = mid + 1;
+            else r = mid;
         }
-    if(l > r){
-        if(r < 1) res = a[l] - b[i];
-        else if(l > m) res = b[i] - a[r];
-        else res = min(abs(a[l] - b[i]), abs(b[i] - a[r]));
+        int k = l;
+        if (a[k] == b[i]) continue;
+        if (k == 0) res += abs(a[k] - b[i]);
+        else if (k == n) res += abs(a[k - 1] - b[i]);
+        else res += min(abs(a[k] - b[i]), abs(a[k - 1] - b[i]));
     }
-        sum += res;
-    }
-    cout << sum;
+    cout << res << endl;
     return 0;
 }
