339b6ee57cbddfe31ec6d61ddf89b38701f8a841
yuzheyaun <13261317705@sina.cn>
2023-06-16 20:29:18 +0800

    add youdao lesson_6_3(finish)

--- C++/YouDao/Lesson_6/lesson_6_3.cpp
+++ C++/YouDao/Lesson_6/lesson_6_3.cpp
@@ -1,6 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[1000005], n, k, sum, maxf;
+long long check(int mid){
+    int num = 0;
+    for(int i = 1; i <= n; i ++){
+        if(a[i] > mid) sum += a[i] - mid;
+    }
+    return num;
+}
 int main(){
-
+    cin >> n >> k;
+    for(int i = 1; i <= n; i ++){
+        cin >> a[i];
+        sum += a[i];
+        if(a[i] > maxf) maxf = a[i];
+    }
+    if(sum < k){
+        cout << 0;
+        return 0;
+    }
+    int l = 1, mid, r = maxf;
+    while(l <= r){
+        mid = (l + r) / 2;
+        if(check(mid) >= k) l = mid + 1;
+        else r = mid - 1;
+    }
+    cout << r;
     return 0;
 }
