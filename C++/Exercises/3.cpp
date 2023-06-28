002f90c64c529e9e964abced2efc4c568d2c02b4
yuzheyaun <13261317705@sina.cn>
2023-06-28 19:30:14 +0800

    add c++ exercise[3].1

---
+++ C++/Exercises/3.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+//正常血压
+struct XY{
+    int ss;
+    int sz;
+} a[105];
+int main(){
+	int n, res = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i ++){
+        cin >> a[i].ss >> a[i].sz;
+        if(a[i].ss >= 90 && a[i].ss <= 140 && a[i].sz >= 60 && a[i].sz <= 90){
+            res ++;
+            continue;
+        }
+        res = 0;
+
+    }
+    cout << res;
+	return 0; 
+} 
