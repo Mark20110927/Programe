fb99ffeeebb0bb293654c72f3f02a66e009ec47e
yuzheyaun <13261317705@sina.cn>
2023-06-29 18:40:13 +0800

    add c++ exercise[4].2

--- C++/Exercises/4.cpp
+++ C++/Exercises/4.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 //买房子
 int main(){
-	int n, k, res = 0, b = 200;
-    cin >> n >> k;
-    float a = k / 100;
+	int n1, k, res = 0, n;
+    float b = 200;
+    cin >> n1 >> k;
+    n = n1;
+    float a = float(k / 10);
     while(res <= 20){
         res ++;
-        if(n >= b) break;
-        else b *= 1 + a;
+        if(n >= b && res <= 20) {
+            cout << res;
+            return 0;
+        }
+        else if(res > 20){
+            cout << "impossible";
+            return 0;
+        }
+        else{
+            b *= 1 + a;
+            n += n1;
+        }
     }
-    if(res <= 20) cout << res;
-    else cout << "impossible";
 	return 0; 
 } 
