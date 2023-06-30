5f344e549d4140e90a66a8340f0b898e0a44f7c5
yuzheyaun <13261317705@sina.cn>
2023-06-30 19:03:03 +0800

    add youdao lesson_7_1(1)

--- C++/YouDao/Lesson_7/lesson_7_1.cpp
+++ C++/YouDao/Lesson_7/lesson_7_1.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[15];
 int main(){
-    
+    int n, x;
+    cin >> n >> x;
+    for(int i = 0; i <= n - 1; i ++) cin >> a[i];
+    int f = a[n];
+    for(int i = n - 1; i >= 0; i --) f = f * x + a[i];
+    cout << f;
     return 0;
 }
