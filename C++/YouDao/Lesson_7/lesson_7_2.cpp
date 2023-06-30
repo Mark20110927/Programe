60dd467fd4bdb84c9e1f2271a268b9e59575e20f
yuzheyaun <13261317705@sina.cn>
2023-06-30 19:23:30 +0800

    add youdao lesson_7_2(1)

--- C++/YouDao/Lesson_7/lesson_7_2.cpp
+++ C++/YouDao/Lesson_7/lesson_7_2.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[100005];
 int main(){
-    
+    int n, q, x, y;
+    cin >> n;
+    for(int i = 1; i <= n; i ++) cin >> a[i];
+    cin >> q;
+    for(int i = 1; i <= q; i ++){
+        int sum = 0;
+        cin >> x >> y;
+        for(int j = x; i <= y; i ++) sum += a[x];
+        cout << sum;
+    }
     return 0;
 }
