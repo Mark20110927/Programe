808164a11e14aab048a77de599c7060e26248c62
yuzheyaun <13261317705@sina.cn>
2023-06-30 20:31:02 +0800

    add youdao lesson_7_4(finish)

--- C++/YouDao/Lesson_7/lesson_7_4.cpp
+++ C++/YouDao/Lesson_7/lesson_7_4.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, s, a[1000005];
 int main(){
-    
+    cin >> n >> s;
+    for(int i = 1; i <= n; i ++) cin >> a[i];
+    sort(a + 1, a + 1 + n);
+    int ans = 0;
+    for(int i = 1; i <= n; i ++){
+        int l = i + 1, r = n, mid;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(a[i] + a[mid] <= s) l = mid + 1;
+            else r = mid - 1;
+        }
+        ans += r - i;
+    }
+    cout << ans;
     return 0;
 }
