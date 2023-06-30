9a22513c939a6909f35ed2db4f3cf82d3989e7fc
yuzheyaun <13261317705@sina.cn>
2023-06-30 20:58:15 +0800

    add youdao lesson_7_5(finish)

--- C++/YouDao/Lesson_7/lesson_7_5.cpp
+++ C++/YouDao/Lesson_7/lesson_7_5.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, w, t, a[605];
 int main(){
-    
+    cin >> n >> w;
+    for(int i = 1; i <= n; i ++){
+        cin >> t;
+        a[t] ++;
+        int p = max(1, i * w / 100);
+        for(int j = 600; j >= 0; j --){
+            p -= a[j];
+            if(p <= 0){
+                cout << j << ' ';
+                break;
+            }
+        }
+    }
     return 0;
 }
