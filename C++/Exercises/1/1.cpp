a6f42a7e9abf6fffe8861bd9a2ee102082c6e970
yuzheyaun <13261317705@sina.cn>
2023-07-08 06:47:41 +0800

    add c++ exercise[1].1

--- C++/Exercises/1/1.cpp
+++ C++/Exercises/1/1.cpp
@@ -1,16 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n, n1;
-    double he = 0, pjz;
-    cin >> n;
-    n1 = n;
-    while(n --){
-        int x;
-        cin >> x;
-        he += x;
+    string s;
+    char c;
+    while(true){
+        cin >> c;
+        s += c;
+        if(c == 'E') break;
     }
-    pjz = he / n1;
-    cout << he << ' ' << fixed << setprecision(5) << pjz;
+    int cnt1 = 0, cnt2 = 0;
+    for(int i = 1; i < s.size() ; i ++){
+        if(s[i] == 'W') cnt1 ++;
+        else cnt1 ++;
+        if(cnt1 == 11 || cnt2 == 11){
+            cout << cnt1 << ':' << cnt2 << endl;
+            cnt1 = 0;
+            cnt2 = 0;
+        }
+        if(s[i] == 'E'){
+            cout << cnt1 << ':' << cnt2 << endl << endl;
+            break;
+        }
+    }
+    for(int i = 1; i < s.size() ; i ++){
+        if(s[i] == 'W') cnt2 ++;
+        else cnt1 ++;
+        if(cnt1 == 21 || cnt2 == 21){
+            cout << cnt1 << ':' << cnt2 << endl;
+            cnt1 = 0;
+            cnt2 = 0;
+        }
+        if(s[i] == 'E'){
+            cout << cnt1 << ':' << cnt2 << endl << endl;
+            break;
+        }
+    }
     return 0;
 }
