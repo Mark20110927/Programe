7544406308fffd155d2e28f4784dc44adbff97c5
yuzheyaun <13261317705@sina.cn>
2023-07-07 15:50:01 +0800

    add python work_project1 V1.1

--- Python/Work_Projects/Project1/main.py
+++ Python/Work_Projects/Project1/main.py
@@ -1,27 +1,37 @@
 import requests
-import pandas as pd
+import json
+from datetime import datetime
 
-def get_okex_kline(symbol, interval, size=200):
-    base_url = "https://www.okex.com/api/spot/v3/instruments/"
-    url = f"{base_url}{symbol}/candles?granularity={interval}&size={size}"
-    response = requests.get(url)
+# OKEX K线数据的URL
+url = "https://www.okex.com/api/v5/market/history-index-candles"
 
-    if response.status_code == 200:
-        data = response.json()
-        df = pd.DataFrame(data, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
-        return df
+date_string = "2023-07-04T00:00:00.000Z"
+date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")  # 转换日期字符串为datetime对象
+
+date_string1 = "2023-07-05T00:00:00.000Z"
+date1 = datetime.strptime(date_string1, "%Y-%m-%dT%H:%M:%S.%fZ")  # 转换日期字符串为datetime对象
+
+# 请求参数
+params = {
+    'instId': 'BTC-USDT',
+    'start': date.isoformat() + 'Z',  # 开始时间
+    'end': date1.isoformat() + 'Z',  # 结束时间
+    'bar': '15m',
+}
+
+# 发起请求
+response = requests.get(url, params=params)
+
+# 检查HTTP状态码
+if response.status_code != 200:
+    print(f"请求失败，状态码：{response.status_code}")
+else:
+    # 获取数据
+    data = json.loads(response.text)
+
+    # 如果API返回错误
+    if 'message' in data:
+        print(f"API返回错误：{data['message']}")
     else:
-        print(f"请求失败，状态码：{response.status_code}")
-        return None
-
-# 先获取最近的200条数据
-df1 = get_okex_kline('BTC-USDT', 60)
-
-# 再获取200条较早的数据
-df2 = get_okex_kline('BTC-USDT', 60)
-
-# 合并两组数据并取出最新的300条
-df = pd.concat([df1, df2])
-df = df.iloc[:300]
-
-print(df)
+        # 打印数据
+        print(data)
