8c72ed8e0709fc8ba7c8d64c506eba7936dba822
yuzheyaun <13261317705@sina.cn>
2023-07-08 07:51:53 +0800

    add c++ exercise[1].finish

--- C++/Exercises/1/1.cpp
+++ C++/Exercises/1/1.cpp
@@ -1,39 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     string s;
     char c;
-    while(true){
+    while (true) {
         cin >> c;
         s += c;
-        if(c == 'E') break;
+        if (c == 'E') break;
     }
+    
     int cnt1 = 0, cnt2 = 0;
-    for(int i = 1; i < s.size() ; i ++){
-        if(s[i] == 'W') cnt1 ++;
-        else cnt1 ++;
-        if(cnt1 == 11 || cnt2 == 11){
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == 'W') cnt1++;
+        else if (s[i] == 'L') cnt2++;
+        
+        if ((cnt1 >= 11 && cnt1 - cnt2 >= 2) || (cnt2 >= 11 && cnt2 - cnt1 >= 2) || s[i] == 'E') {
             cout << cnt1 << ':' << cnt2 << endl;
             cnt1 = 0;
             cnt2 = 0;
-        }
-        if(s[i] == 'E'){
-            cout << cnt1 << ':' << cnt2 << endl << endl;
-            break;
+            if (s[i] == 'E') break;
         }
     }
-    for(int i = 1; i < s.size() ; i ++){
-        if(s[i] == 'W') cnt2 ++;
-        else cnt1 ++;
-        if(cnt1 == 21 || cnt2 == 21){
+    cout << endl;
+    
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == 'W') cnt1++;
+        else if (s[i] == 'L') cnt2++;
+        
+        if ((cnt1 >= 21 && cnt1 - cnt2 >= 2) || (cnt2 >= 21 && cnt2 - cnt1 >= 2) || s[i] == 'E') {
             cout << cnt1 << ':' << cnt2 << endl;
             cnt1 = 0;
             cnt2 = 0;
-        }
-        if(s[i] == 'E'){
-            cout << cnt1 << ':' << cnt2 << endl << endl;
-            break;
+            if (s[i] == 'E') break;
         }
     }
+    
     return 0;
 }
