f66dc1347351c0be1213d8eafd2628524f1187d6
yuzheyaun <13261317705@sina.cn>
2023-07-12 07:54:05 +0800

    add python work_project1 V1.8

--- Python/Work_Projects/Project1/main.py
+++ Python/Work_Projects/Project1/main.py
@@ -2,14 +2,14 @@ import requests
 import json
 from datetime import datetime
 url = "https://www.okex.com/api/v5/market/history-index-candles"
-date_string = "2023-07-03T00:00:00.000Z"#str(input("请输入开始时间：")) #
-date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
-date_string1 = "2023-07-04T00:00:00.000Z"#str(input("请输入结束时间: "))#
-date1 = datetime.strptime(date_string1, "%Y-%m-%dT%H:%M:%S.%fZ")
+# date_string = "2023-07-01T00:00:00.000Z"#str(input("请输入开始时间：")) #
+# date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+# date_string1 = "2023-07-02T00:00:00.000Z"#str(input("请输入结束时间: "))#
+# date1 = datetime.strptime(date_string1, "%Y-%m-%dT%H:%M:%S.%fZ")
 params = {
     'instId': 'BTC-USDT',#input("请输入加密货币的类型："), #,
-    'start': date.isoformat() + 'Z',
-    'end': date1.isoformat() + 'Z',
+    'start': "2023-07-05T00:00:00.000Z",
+    'end': "2023-07-06T00:00:00.000Z",
     'bar': '15m',
 }
 response = requests.get(url, params=params)
