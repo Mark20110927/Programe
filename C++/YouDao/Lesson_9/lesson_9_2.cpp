5d845aa3fe50ef0fdebd244fcab66814ec479797
yuzheyaun <13261317705@sina.cn>
2023-07-12 18:40:39 +0800

    add youdao lesson_9_2(1)

--- C++/YouDao/Lesson_9/lesson_9_2.cpp
+++ C++/YouDao/Lesson_9/lesson_9_2.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[20005];
 int main(){
-
+    int n, b;
+    long long sum = 0;
+    for(int i = 1; i <= n; i ++) cin >> a[i];
+    sort(a + 1, a + 1 + n, greater<int>());
+    for(int i = 1; i <= n; i ++){
+        sum += a[i];
+        if(sum >= b){
+            cout << i;
+            return 0;
+        }
+    }
     return 0;
 }
