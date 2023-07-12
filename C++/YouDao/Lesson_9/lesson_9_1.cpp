42800df5b75be71d0dfff00ff3ed8dbabbbc0a23
yuzheyaun <13261317705@sina.cn>
2023-07-12 18:22:13 +0800

    add youdao lesson_9_1(1)

--- C++/YouDao/Lesson_9/lesson_9_1.cpp
+++ C++/YouDao/Lesson_9/lesson_9_1.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n, a;
+    cin >> n;
+    int money[4] = {20, 10, 5, 1};
+    for(int i = 1; i <= 4; i ++){
+        a = n % money[i];
+        cout << a << ' ';
+        n %= money[i];
+    }
     return 0;
 }
