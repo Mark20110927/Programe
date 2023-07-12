b7b840557492ad0d1916080fa233b6c1871636a6
yuzheyaun <13261317705@sina.cn>
2023-07-12 20:14:49 +0800

    add youdao lesson_9_4(FINISH)

--- C++/YouDao/Lesson_9/lesson_9_4.cpp
+++ C++/YouDao/Lesson_9/lesson_9_4.cpp
@@ -5,22 +5,18 @@ struct jz{
     int end;
 }a[105];
 bool cmp(jz x, jz y){
-    return x.start < y.start;
+    return x.end < y.end;
 }
 int main(){
-    int n, res = 1;
+    int n, res = 0;
     cin >> n;
     for(int  i = 1; i <= n; i ++) cin >> a[i].start >> a[i].end;
     sort(a + 1, a + 1 + n, cmp);
-    int ne = a[1].end;
-    for(int i = 2; i <= n; i ++){
-        if(a[i].start >= ne && a[i + 1].start > a[i].start){
+    int ne = 0;
+    for(int i = 1; i <= n; i ++){
+        if(a[i].start >= ne){
             res ++;
             ne = a[i].end;
-        }
-        else if(a[i + 1].start > a[i].start){
-            res ++;
-            ne = a[i].end < a[i + 1].end ? a[i + 1].end : a[i].end;
         }
     }
     cout << res;
