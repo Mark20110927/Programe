9fcbfbe5c2a223c2b5349fa037141ed4da727f36
yuzheyaun <13261317705@sina.cn>
2023-07-14 09:26:10 +0800

    add youdao lesson_9_home_2(finsh)

--- C++/YouDao/Lesson_9/lesson_9_homework_2.cpp
+++ C++/YouDao/Lesson_9/lesson_9_homework_2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 struct poeple{
     int time;
     int id;
-}a[1000005];
+}a[1005];
 int n;
 double sum;
 bool cmp(poeple a, poeple b){
@@ -13,13 +13,14 @@ int main(){
     cin >> n;
     for(int i = 1; i <= n; i ++){
         cin >> a[i].time;
-        sum += a[i].time;
         a[i].id = i;
     }
     sort(a + 1, a + 1 + n, cmp);
+    double wait_time = 0;
     for(int i = 1; i <= n; i ++){
+        wait_time += (n - i) * a[i].time;
         cout << a[i].id << ' '; 
     }
-    cout << endl << (double) sum / n;
+    cout << endl << fixed << setprecision(2) << wait_time / n << endl;
     return 0;
 }
