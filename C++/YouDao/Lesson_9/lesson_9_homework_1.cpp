c4beee67b20d2a6fdad83d642900961d7b4059f2
yuzheyaun <13261317705@sina.cn>
2023-07-14 08:09:30 +0800

    add youdao lesson_9_home_1(1)

---
+++ C++/YouDao/Lesson_9/lesson_9_homework_1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[15], b[15], num;
+bool flag, c[15];
+int main(){
+    for(int i = 1; i <= 10;i ++){
+        cin >>a[i];
+        if(a[i] == 1) flag = true;
+    }
+    sort(a + 1, a + 1 + 10);
+    if(flag){
+        b[1] = 1;
+        for(int i = 2; i <= 10; i ++) b[i] = a[i];
+    }
+    else{
+        int minf = 1000000;
+        for(int i = 1; i <= 10; i ++){
+            if(a[i] > 1 && a[i] < minf){
+                num = i;
+                minf = a[i];
+            }
+        }
+        b[1] = minf;
+        c[num] = true;
+        for(int i = 1; i <= 10 ;i ++){
+            if(!c[i]) b[i] = c[i];
+        }
+    }
+    for(int i = 1; i <= 10; i ++) cout << b[i] << ' ';
+    return 0;
+}
