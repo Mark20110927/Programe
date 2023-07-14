4df6b4af516a811e84acedbcfe5411c99feb6ee2
yuzheyaun <13261317705@sina.cn>
2023-07-14 09:04:43 +0800

    add yyoudao lesson_9_home_1(5)

--- C++/YouDao/Lesson_9/lesson_9_homework_1.cpp
+++ C++/YouDao/Lesson_9/lesson_9_homework_1.cpp
@@ -15,28 +15,12 @@ int main(){
             minf = a[i];
         }
     }
-    // cout << minf;
-    if(flag){
-        b[1] = minf;
-        c[num] = true;
-        for(int i = 1; i <= 10 ;i ++){
-            if(i != num) b[i] = c[i];
-        }
+    cout << minf;
+    b[1] = minf;
+    c[num] = true;
+    for(int i = 1; i <= 10 ;i ++){
+        if(!c[i]) b[i] = a[i];
     }
-    else{
-        int minf = 1000000;
-        for(int i = 1; i <= 10; i ++){
-            if(a[i] > 1 && a[i] < minf){
-                num = i;
-                minf = a[i];
-            }
-        }
-        b[1] = minf;
-        c[num] = true;
-        for(int i = 1; i <= 10 ;i ++){
-            if(!c[i]) b[i] = c[i];
-        }
-    }
-    for(int i = 1; i <= 10; i ++) cout << b[i];
+    for(int i = 1; i < 10; i ++) cout << b[i];
     return 0;
 }
